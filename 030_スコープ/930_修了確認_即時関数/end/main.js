4c6e9481d9d3a148171c9ad67784831e6a7cea82
soikawa <soikawa0108@gmail.com>
2024-01-08 23:50:53 +0900

    修正

--- 030_スコープ/930_修了確認_即時関数/end/main.js
+++ 030_スコープ/930_修了確認_即時関数/end/main.js
@@ -3,32 +3,30 @@
  * クロージャーの問題で作成した以下のcalcFactoryを即時関数
  * で書き直してみてください。
  */
-const calc = (function(val) {
+const calc = (function (val) {
     return {
-        plus: function(target) {
+        plus: function (target) {
             const newVal = val + target;
             console.log(`${val} + ${target} = ${newVal}`);
             val = newVal;
         },
-        minus: function(target) {
+        minus: function (target) {
             const newVal = val - target;
             console.log(`${val} - ${target} = ${newVal}`);
             val = newVal;
         },
-        multiply: function(target) {
+        multiply: function (target) {
             const newVal = val * target;
             console.log(`${val} x ${target} = ${newVal}`);
             val = newVal;
         },
-        divide: function(target) {
+        divide: function (target) {
             const newVal = val / target;
             console.log(`${val} / ${target} = ${newVal}`);
             val = newVal;
         },
     }
 })(10);
-
-
 calc.plus(5);
 calc.minus(3);
 calc.multiply(3);
