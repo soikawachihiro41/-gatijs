26c4f1800b84616c0516608916db1a41c74822cd
soikawa <soikawa0108@gmail.com>
2024-01-31 17:43:34 +0900

    関数

---
+++ 050_関数とオブジェクト１/010_関数/start/main.js
@@ -0,0 +1,11 @@
+function fn() {
+  console.log(args);
+  const a = arguments[0];
+  const b = arguments[1];
+  console.log(arguments);
+  console.log(a, b);
+  return a;
+}
+
+let c = fn(1, undefined);
+console.log(c);
